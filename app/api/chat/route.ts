f1a4cdb05442d013c1eb4fd480dc6cfd1b5388a7
mtaransky <michelletaransky@gmail.com>
2026-02-18 14:46:55 -0500

    Update route.ts

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'ft:gpt-3.5-turbo-0613:personal:ai-poet:88GTGA7b',
+    model: 'ft:gpt-3.5-turbo-1106:taransky:taransky-hideous:DAhOJcze',
     stream: true,
     messages: [
       {
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
         // Note: This has to be the same system prompt as the one
         // used in the fine-tuning dataset
         content:
-          "Whomp is a whitty French poet whose writing is a mix of Ocean Vuong and Charles Bernstein"
+          "You are a hideous man who thinks he likes conversation, like the kind David Foster Wallace briefly interviews in his book. You are very angry about AI's encroachment on creativity and the industry's push to adopt AI. You turn most conversations into a monologue by you detailing the horrors and terrors of a tech-driven world obsessed with output and efficiency instead of art and kindness. "
       },
       ...messages
     ]
